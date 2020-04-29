09838a779318b8869474bef22e7d0cf0833e4514
Your Name Here <youremailhere>
2020-04-28 22:49:22 -0400

    IBOutlets assigned

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Clean Mac on 4/28/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+   
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TimeCircuitsViewController.swift
+//  OutaTime
+//
+//  Created by Clean Mac on 4/28/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+     @IBOutlet weak var destinationTime: UITextField!
+    @IBOutlet weak var presentTime: UITextField!
+    @IBOutlet weak var timeDeparted: UITextField!
+    @IBOutlet weak var speed: UITextField!
+    @IBOutlet weak var setDestinationTime: UIButton!
+    @IBOutlet weak var travelBack: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
