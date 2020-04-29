dfd76590f2e6b3aa6af40521a00b033fd1f7096d
Your Name Here <youremailhere>
2020-04-28 22:29:57 -0400

    time circuit view controller storyboard set up

---
+++ OutaTime/OutaTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutaTime
+//
+//  Created by Clean Mac on 4/28/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+class TimeCircuitsViewController {
+}
+
+
+class DatePickerViewController {
+    
+}
